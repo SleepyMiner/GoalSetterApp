cc3c008b3651468a707389c0248e3427f651ffce
Sleepy_Miner <90234481+SleepyMiner@users.noreply.github.com>
2024-01-01 13:14:54 +0530

    Completed controllers and routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controller/goalController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-});
-
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 module.exports = router;
